518df51632a266a96713204b1faeb6348d41b16d
Takashi <bt.tengan@gmail.com>
2021-10-02 16:51:11 -0300

    Access to search sheet by name

--- Assets/Scripts/MusicSheet/MusicSheetManager.cs
+++ Assets/Scripts/MusicSheet/MusicSheetManager.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class MusicSheetManager : SingletonMonobehaviour<MusicSheetManager>
 {
     public List<MusicSheet> MusicSheetList => _musicSheetList;
+    public Dictionary<string, MusicSheet> MusicSheetDict => _musicSheetDict;
     
     List<MusicSheet> _musicSheetList = new List<MusicSheet>();
+    Dictionary<string, MusicSheet> _musicSheetDict = new Dictionary<string, MusicSheet>();
     
     void Start() {
         MusicSheetFileManager.Instance.LoadedFiles += LoadMusicSheets;
@@ -20,6 +22,7 @@ public class MusicSheetManager : SingletonMonobehaviour<MusicSheetManager>
             MusicSheet musicSheet = file.GetMusicSheet();
             if (musicSheet != null) {
                 _musicSheetList.Add(musicSheet);
+                _musicSheetDict.Add(musicSheet.Name, musicSheet);
             }
         }
     }
